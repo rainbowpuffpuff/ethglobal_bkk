914e7f4026b03516cd1d64ac147e6bdb7ebc50cb
0xAstraea <marazza.matias@gmail.com>
2024-12-25 21:48:20 +0700

    Enhance Next.js configuration with Serwist integration and service worker support
    - Updated `.gitignore` to include service worker files.
    - Refactored `next.config.js` to support dynamic configuration based on the build phase, integrating Serwist for service worker management.
    - Added `sw.ts` for service worker implementation.
    - Updated `package.json` and `package-lock.json` to include `@serwist/next` and `serwist` dependencies.
    - Modified `tsconfig.json` to include web worker types and adjusted exclusions.
    - Enhanced metadata in `layout.tsx` for improved SEO and application details.
    - Updated `manifest.json` for the new application name and configuration.
    
    This commit introduces service worker capabilities and improves the overall configuration for better performance and compatibility.

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -2,16 +2,54 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { ScaffoldEthAppWithProviders } from "~~/components/ScaffoldEthAppWithProviders";
 import { ThemeProvider } from "~~/components/ThemeProvider";
 import "~~/styles/globals.css";
-import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+import type { Metadata, Viewport } from "next";
 
-export const metadata = getMetadata({
-  title: "Think2Earn",
-  description: "Built with 🏗 Scaffold-ETH 2",
-});
+const APP_NAME = "think2earn";
+const APP_DEFAULT_TITLE = "think2earn";
+const APP_TITLE_TEMPLATE = "%s - think2earn";
+const APP_DESCRIPTION = "think2earn";
 
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html suppressHydrationWarning lang="en" dir="ltr">
       <body className="w-[100vw] overflow-x-hidden">
         <ThemeProvider attribute="class">
           <div className="bg-background">
--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -1,19 +1,35 @@
 // @ts-check
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
-  },
-  eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
-  },
-  webpack: config => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
-};
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
 
-module.exports = nextConfig;
+/** @type {(phase: string, defaultConfig: import("next").NextConfig) => Promise<import("next").NextConfig>} */
+module.exports = async (phase) => {
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    reactStrictMode: true,
+    typescript: {
+      ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    },
+    eslint: {
+      ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    },
+    webpack: config => {
+      config.resolve.fallback = { fs: false, net: false, tls: false };
+      config.externals.push("pino-pretty", "lokijs", "encoding");
+      return config;
+    },
+  };
+
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      swSrc: "app/sw.ts",
+      swDest: "public/sw.js",
+    });
+    return withSerwist(nextConfig);
+  }
+
+  return nextConfig;
+};
